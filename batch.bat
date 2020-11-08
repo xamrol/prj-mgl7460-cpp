640984ac88cd895a1862342f4f460dc130834e46
Arol Gbeto-fia <g.senami94@gmail.com>
2020-11-07 20:17:14 -0500

    Ajout de l'analyse de code

--- batch.bat
+++ batch.bat
@@ -2,4 +2,5 @@
 "C:\MinGW\bin\g++.exe" -c main.cpp
 "C:\MinGW\bin\g++.exe" -o uptime-app calculator.o main.o
 uptime-app.exe
-
+"C:\Program Files\Cppcheck\cppcheck.exe" --check-config --xml --output-file=cppCheckResults.xml --verbose .
+start CppCheckResults.xml
